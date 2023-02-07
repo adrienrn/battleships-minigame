483ecefba6c4d4a611f5dd12af35957d23e62c7a
Adrien Ricart-Noblet <adrien@sesamecare.com>
2023-02-07 18:31:34 +0100

    Cleanup naming

--- src/components/Board/Board.tsx
+++ src/components/Board/Board.tsx
@@ -1,21 +1,21 @@
 import React from "react";
 import { ReactComponent as ShipBattleshipIcon } from "../../assets/ship-battleship.svg";
 import { ReactComponent as ShipDestroyerIcon } from "../../assets/ship-destroyer.svg";
-import { BoardCell, BoardCellState } from "../../types/Board";
+import { BoardCell as BoardCellType, BoardCellState } from "../../types/Board";
 import { Ship, ShipType } from "../../types/Ship";
 import { TileWithFog, TileWithHit, TileWithMiss } from "../Tile/Tile";
 import styles from "./Board.module.css";
 
 export const Board: React.FC<{
-  cells: BoardCell[];
+  cells: BoardCellType[];
   debug?: boolean;
   ships: Ship[];
   shots?: number[];
 }> = ({ cells = [], debug = false, ships = [], shots = [] }) => {
   return (
-    <div className={styles["Board__grid"]}>
+    <div className={styles["Board"]}>
       {cells.map((currentCell) => (
-        <BoardGridItem
+        <BoardCell
           cell={currentCell}
           debug={debug}
           key={currentCell.id}
@@ -27,8 +27,8 @@ export const Board: React.FC<{
   );
 };
 
-const BoardGridItem: React.FC<{
-  cell: BoardCell;
+const BoardCell: React.FC<{
+  cell: BoardCellType;
   debug?: boolean;
   ships?: Ship[];
   shots?: number[];
@@ -36,17 +36,17 @@ const BoardGridItem: React.FC<{
   const cellState = computeTileState(cell, ships, shots);
 
   return (
-    <div className={styles["Board__grid__item"]}>
+    <div className={styles["Board__cell"]}>
       {cellState === BoardCellState.MISS && <TileWithMiss cell={cell} />}
       {cellState === BoardCellState.HIT && <TileWithHit cell={cell} />}
       {cellState === BoardCellState.UNKNOWN && <TileWithFog cell={cell} />}
-      {debug && <BoardCellDebugOverlay cell={cell} ships={ships} />}
+      {debug && <BoardCellDebug cell={cell} ships={ships} />}
     </div>
   );
 };
 
-export const BoardCellDebugOverlay: React.FC<{
-  cell: BoardCell;
+export const BoardCellDebug: React.FC<{
+  cell: BoardCellType;
   ships: Ship[];
 }> = ({ cell, ships }) => {
   const matchingShip: Ship | undefined = findMatchingShip(cell, ships);
@@ -55,22 +55,22 @@ export const BoardCellDebugOverlay: React.FC<{
   }
 
   return (
-    <span className={styles["Board__grid__ship-marker"]}>
+    <span className={styles["Board__cell__ship-marker"]}>
       {matchingShip.type === ShipType.BATTLESHIP && (
         <ShipBattleshipIcon
-          className={styles["Board__grid__ship-marker__icon"]}
+          className={styles["Board__cell__ship-marker__icon"]}
         />
       )}
       {matchingShip.type === ShipType.DESTROYER && (
         <ShipDestroyerIcon
-          className={styles["Board__grid__ship-marker__icon"]}
+          className={styles["Board__cell__ship-marker__icon"]}
         />
       )}
     </span>
   );
 };
 
-function computeTileState(cell: BoardCell, ships: Ship[], shots: number[]) {
+function computeTileState(cell: BoardCellType, ships: Ship[], shots: number[]) {
   const matchingShip = findMatchingShip(cell, ships);
   const hasCellBeenShot = shots.includes(cell.id);
 
@@ -84,7 +84,7 @@ function computeTileState(cell: BoardCell, ships: Ship[], shots: number[]) {
   return BoardCellState.UNKNOWN;
 }
 
-function findMatchingShip(cell: BoardCell, ships: Ship[]) {
+function findMatchingShip(cell: BoardCellType, ships: Ship[]) {
   return ships.find((value) => {
     return value.cells.some((e: any) => e === cell.id);
   });
